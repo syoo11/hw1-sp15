7bbe932fab9b9c9eb4409dba45877ea4cdca3757
syoo11 <vagrant@rails-dev-box>
2015-02-19 09:46:23 +0000

    change name Routing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def home
+  end
+
+  def about
+  	@major="Applied Math"
+  	@age=12
+  	@song="Stay"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,4 @@
 Rails.application.routes.draw do
+	root 'application#home'
+	get '/about', to: 'application#about'
 end
